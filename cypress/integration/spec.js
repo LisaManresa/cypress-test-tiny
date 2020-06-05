116b8d70611fb32bdac44937e3763922aec00a6b
Lisa Manresa <Lisa.C.Manresa@gmail.com>
2020-06-05 08:53:53 -0700

    Test demonstrating `have.value` failure on <progress> and <meter>

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,6 +1,77 @@
 /// <reference types="cypress" />
 describe('page', () => {
-  it('works', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+  context('have.value', function() {
+    it('succeeds when checking value of input element', () => {
+      cy.visit('index.html')
+
+      cy
+        .get('#textInput')
+        .should('have.value', 50)
+        .should('have.value', '50');
+
+      cy
+        .get('#numInput')
+        .should('have.value', 50)
+        .should('have.value', '50');
+
+        cy
+        .get('#rangeInput')
+        .should('have.value', 50)
+        .should('have.value', '50');
+
+    });
+
+    it('fails on check of progress element value with expected value a Number', () => {
+      cy.visit('index.html')
+
+      cy
+        .get('#progressEl')
+        .should('have.value', 50);
+    });
+
+    it('fails on check of progress element value with expected value a String', () => {
+      cy.visit('index.html')
+
+      cy
+        .get('#progressEl')
+        .should('have.value', '50');
+    });
+
+
+    it('fails on check of meter element value with expected value a Number', () => {
+      cy.visit('index.html')
+
+      cy
+        .get('#meterEl')
+        .should('have.value', 50);
+    });
+
+    it('fails on check of meter element value with expected value a String', () => {
+      cy.visit('index.html')
+
+      cy
+        .get('#meterEl')
+        .should('have.value', '50');
+    });
+  });
+
+  context('have.attr', function() {
+    it('succeeds on check of progress element value using `have.attr`', () => {
+      cy.visit('index.html')
+
+      cy
+        .get('#progressEl')
+        .should('have.attr', 'value', '50')
+        .should('have.attr', 'value', 50);
+    });
+
+    it('succeeds on check of meter element value using `have.attr`', () => {
+      cy.visit('index.html')
+
+      cy
+        .get('#meterEl')
+        .should('have.attr', 'value', '50')
+        .should('have.attr', 'value', 50);
+    });
+  });
+});
